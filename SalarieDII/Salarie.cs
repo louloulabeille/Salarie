73449b9a88f1f477b6aafc072c157262e608fb47
louloulabeille <louloulabeille@hotmail.com>
2020-09-14 21:48:35 +0200

    Fin de la première partie de l'exercice

--- SalarieDII/Salarie.cs
+++ SalarieDII/Salarie.cs
@@ -14,27 +14,45 @@ namespace SalarieDII
         private DateTime _dateNaissance;
 
         public const string _patternMatricule = @"^\d{2}[A-Z]{3}\d{2}$";
-        public const string _patternNom = @"[a-zA-Z][a-zA-Z]";
+        public const string _patternNom = @"[A-Za-z]{3,30}?";
+        public const double _maxTaux = 0.60;
+        public const double _minTaux = 0.0;
+        public const int _anneeMin = -15;
+        
 
         public string Matricule { get => _matricule;
             set
             {
-                _matricule = VerifMatricule(value) ? value : null;
-                    //throw new Exception(string.Format($"La saisie du matricule est incorect, il doit être de forme '12DFG15' et vous avez saisie '{value}'. "));
+                _matricule = isVerifMatricule(value) ? value : throw new Exception(string.Format($"La saisie du matricule est incorect, il doit être de forme '12DFG15' et vous avez saisie '{value}'. "));
             }
         }
 
         public string Nom { get => _nom;
             set
             {
-                _nom = VerifNom(value) ? value : null;
-                    //throw new Exception(string.Format($"La saisie du matricule est incorect, il doit comporter de 3 à 30 caractères non décimal et vous avez saisie '{value}'. "));
+                _nom = isVerifNomPrenom(value) ? value : throw new Exception(string.Format($"La saisie du nom est incorect, il doit comporter de 3 à 30 caractères non décimal et vous avez saisie '{value}'. "));
             }
         }
-        public string Prenom { get => _prenom; set => _prenom = value; }
+        public string Prenom { get => _prenom;
+            set
+            {
+                _prenom = isVerifNomPrenom(value) ? value : throw new Exception(string.Format($"La saisie du prénom est incorect, il doit comporter de 3 à 30 caractères non décimal et vous avez saisie '{value}'. "));
+            }
+        }
         public double SalaireBrut { get => _salaireBrut; set => _salaireBrut = value; }
-        public double TauxCS { get => _tauxCS; set => _tauxCS = value; }
-        public DateTime DateNaissance { get => _dateNaissance; set => _dateNaissance = value; }
+        public double TauxCS { get => _tauxCS;
+            set
+            {
+                _tauxCS = isVerifTaux( value )? value: throw new Exception(string.Format($"La saisie du taux est invalide, il doit être compris entre {_minTaux} et {_maxTaux}."));
+            }
+        }
+        public DateTime DateNaissance { get => _dateNaissance;
+            set
+            {
+                _dateNaissance = isVerifDateNaissance(value) ? value : throw new Exception(string.Format("La date est incorrecte, elle doit être supérieur à 01/01/1900 et inférieur de 15 ans à la date du jour"));
+            }
+        
+        }
         public double SalaireNet {      // calcul du salaire Nets
             get
             {
@@ -54,23 +72,45 @@ namespace SalarieDII
         /// </summary>
         /// <param name="mat">chaine de caractère a vérifié</param>
         /// <returns></returns>
-        public static bool VerifMatricule ( string mat )
+        public static bool isVerifMatricule ( string mat )
         {
             Regex rgx = new Regex(_patternMatricule);
             return rgx.IsMatch(mat);
         }
-
         /// <summary>
         /// verifie si le nom a entre 3 et 30 caractères
         /// et non numérique. Voir la constante _patternNom
         /// </summary>
-        /// <param name="name">nom de la personne</param>
+        /// <param name="name">nom ou le prénom de la personne</param>
         /// <returns></returns>
-        public static bool VerifNom ( string name )
+        public static bool isVerifNomPrenom ( string name )
         {
             Regex rgx = new Regex(_patternNom);
             return rgx.IsMatch(name);
         }
+        /// <summary>
+        /// Verifis le taux s'il est compris entre 0 et 0.6
+        /// c'est a dire entre le nimTaux et maxTaux
+        /// </summary>
+        /// <param name="taux"> taux à vérifier</param>
+        /// <returns></returns>
+        public static bool isVerifTaux ( double taux )
+        {
+            return taux >= _minTaux & taux <= _maxTaux;
+        }
 
+        public static bool isVerifDateNaissance ( DateTime date )
+        {
+            DateTime dateJour = DateTime.Now;
+            DateTime minDateNaissance = new DateTime(1900, 1, 1);
+            dateJour = dateJour.AddYears(_anneeMin);
+
+            if (DateTime.Compare(minDateNaissance, date) <= 0 && dateJour.CompareTo(date) >= 0)
+            {
+                return true;
+            }
+            return false;
+            
+        } 
     }
 }
